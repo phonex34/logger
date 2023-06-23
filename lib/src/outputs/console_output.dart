3ea55e345cce1f82d293cc7227dde712e0ec6cbd
phonex34 <peon.phonex@gmail.com>
2023-06-23 23:21:22 +0700

    add color ios

--- lib/src/outputs/console_output.dart
+++ lib/src/outputs/console_output.dart
@@ -1,12 +1,19 @@
-import 'package:logger/src/log_output.dart';
-import 'package:logger/src/logger.dart';
+import 'dart:developer' as developer;
+import 'dart:io';
+
+import 'package:logger/logger.dart';
 
 /// Default implementation of [LogOutput].
 ///
 /// It sends everything to the system console.
 class ConsoleOutput extends LogOutput {
+  bool isReleaseMode = bool.fromEnvironment('dart.vm.product');
   @override
   void output(OutputEvent event) {
-    event.lines.forEach(print);
+    if (isReleaseMode || !Platform.isIOS) {
+      event.lines.forEach(print);
+    } else {
+      event.lines.forEach(developer.log);
+    }
   }
 }
